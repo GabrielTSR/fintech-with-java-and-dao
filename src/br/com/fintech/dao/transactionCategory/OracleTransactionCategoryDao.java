309d335acb90d7d3caab84e186e9e8bab28e39a2
als1001 <138945372+als1001@users.noreply.github.com>
2023-10-07 15:47:34 -0300

    Update OracleTransactionCategoryDao.java

--- src/br/com/fintech/dao/transactionCategory/OracleTransactionCategoryDao.java
+++ src/br/com/fintech/dao/transactionCategory/OracleTransactionCategoryDao.java
@@ -1,11 +1,14 @@
 package br.com.fintech.dao.transactionCategory;
-
+import java.util.List;
+import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.sql.ResultSet;
 
 import br.com.fintech.jdbc.FintechDBManager;
 import br.com.fintech.model.TransactionCategory;
@@ -40,4 +43,44 @@ public class OracleTransactionCategoryDao implements TransactionCategoryDAO {
 			}
 		}
 	}
+	
+	public List<TransactionCategory> getAll() {
+	    Connection connection = null;
+	    PreparedStatement stmt = null;
+	    ResultSet resultSet = null;
+	    List<TransactionCategory> transactionCategories = new ArrayList<>();
+
+	    try {
+	        connection = FintechDBManager.obterConexao();
+	        String query = "SELECT ID, DESCRIPTION, CREATED_AT FROM TAB_TRANSACTION_CATEGORY";
+	        stmt = connection.prepareStatement(query);
+	        resultSet = stmt.executeQuery();
+
+	        while (resultSet.next()) {
+	            int id = resultSet.getInt("ID");
+	            String description = resultSet.getString("DESCRIPTION");
+	            LocalDateTime createdAt = resultSet.getTimestamp("CREATED_AT").toLocalDateTime();
+	            TransactionCategory category = new TransactionCategory(description, createdAt, id);
+	            transactionCategories.add(category);
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	            if (resultSet != null) {
+	                resultSet.close();
+	            }
+	            if (stmt != null) {
+	                stmt.close();
+	            }
+	            if (connection != null) {
+	                connection.close();
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    return transactionCategories;
+	}
+
 }
