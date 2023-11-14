1252060b4120a4334831911a2ad1c3df5367db1c
Gustavo Torrente <danielapdon@gmail.com>
2023-11-14 19:15:43 -0300

    ajuste sevlet

--- src/main/java/br/com/fintech/controller/UserServlet.java
+++ src/main/java/br/com/fintech/controller/UserServlet.java
@@ -1,12 +1,12 @@
 package br.com.fintech.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import br.com.fintech.dao.user.UserDAO;
 import br.com.fintech.exception.DBException;
@@ -32,12 +32,8 @@ public class UserServlet extends HttpServlet {
 	        User user = new User(name, email, password, 0);
 
 	        dao.insert(user);
-	        
-	        HttpSession session = request.getSession();
-            session.setAttribute("email", email);
-            session.setAttribute("id", user.getId());
 
-	        request.getRequestDispatcher("home.jsp").forward(request, response);
+	        request.getRequestDispatcher("login.jsp").forward(request, response);
 	    } catch (DBException db) {
 	        db.printStackTrace();
 	        request.setAttribute("error", "Erro ao cadastrar");
